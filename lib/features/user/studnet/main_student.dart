01fd5bae92eec7238c2c41a802e344275d17fd1a
adhamAbuAllan <needforspeedun3@gmail.com>
2024-07-05 19:57:19 +0300

    last commit 7/5/2024 testing mode : create apartment and insert advantages is done! Note , the data is put manually , and the advantages is same. but the apartmentId is programmatically inserting

--- lib/features/user/studnet/main_student.dart
+++ lib/features/user/studnet/main_student.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/cupertino.dart';
@@ -6,13 +8,19 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:ween_blaqe/constants/strings.dart';
+import 'package:ween_blaqe/controller/get_controllers.dart';
+import 'package:ween_blaqe/controller/models_controller/advantages_model_controller.dart';
 import 'package:ween_blaqe/core/utils/funcations/route_pages/push_routes.dart';
+import 'package:http/http.dart' as http;
+import 'package:ween_blaqe/data_containers/add_ad_data_container.dart';
 
+import '../../../api/apartments_api/one_apartment.dart';
 import '../../../controller/student_controller/student_controller.dart';
 import '../../../core/utils/funcations/go_url_launcher_methodes/go_to_whatsapp_method.dart';
 import '../../../core/widgets/alirt_class_widget.dart';
 import '../../../core/widgets/bottom_nav_bar/bottom_nav_of_student.dart';
 import 'package:ween_blaqe/constants/nums.dart';
+import '../../../main.dart';
 import '../../../sesstion/new_session.dart';
 import '../../widgets_before_user_reg/account_before_login.dart';
 import 'from_master_home_to_show_more/new_master_home.dart';
@@ -38,6 +46,7 @@ class _MainStudentState extends State<MainStudent> {
 
   var studentController = StudentController();
   int index = 0;
+  String apartmentIdLocal = "";
 
   StudentController controller = Get.find();
 
@@ -355,7 +364,18 @@ class _MainStudentState extends State<MainStudent> {
               NewSession.get("logged", "") == ""
                   ? alert("يرجى تسجيل الدخول لإنشاء إعلانك","قم بتسجيل الدخول أولا لتتمكن من إضافة الشقة الخاصة بك","حسنًا")
 
-                  : myPushName(context, MyPagesRoutes.step1);
+                  :createApartment();
+
+              // createApartmentAndInsertAdvantages();
+              // }
+              if(apartmentIdLocal!=""){
+                setState(() {
+                  apartmentModelController.apartmentId = apartmentIdLocal;
+
+                });
+              }
+
+              // myPushName(context, MyPagesRoutes.step1);
             },
             child: const Icon(
               Icons.add_home_outlined,
@@ -378,7 +398,194 @@ class _MainStudentState extends State<MainStudent> {
       pushToNoInternetPage();
     }
   }
+  Future<void>createApartmentAndInsertAdvantages()async{
+    await createApartment();
+    debugPrint("create apartment is done!");
 
+    // if(AddAdDataContainer.id != null){
+
+  }
+  Future<DataOfOneApartment?>createApartment(
+      // String address,
+      // int bathRooms,
+      // int rooms,
+      // int price,
+      //
+      //   //be careful don't delete it!!!
+      //   //-------------------
+      //   // TypeOfApartment typeOfApartment,
+      //   //-------------------
+      //   //be careful don't delete it!!!
+      //   //-------------------
+      //   // City city,
+      //   //-------------------
+      //
+      // City? city,
+      // int squareMeters,
+      // TypeOfApartment? type,
+      // String title,
+      // String description,
+      // int countOfStudent,
+      // String? email,
+      // User ? ownerId,
+      // DropDownTypeOfUser typeId
+      // String value
+      ) async {
+
+    // try{
+    var url = Uri.parse(ServerWeenBalaqee.apartmentAdd);
+    var token = (await sp).get("token");
+
+    if (token != null) {
+      final headers = {
+        'Authorization':
+        // 'Bearer ${apartmentModelController.ownerToken}',
+        // 'Bearer 59|kO3wxOnjan40Ps4OO7D14nC0tt4tUAhq5oABl7f23256a9b0',
+        'Bearer $token',
+        'Content-Type': 'application/json',
+      };
+
+      debugPrint("the token is : $token");
+
+      var body = jsonEncode({
+        "location":  "testing",
+        "bathrooms": "09",
+        // bathRooms.toString(),
+        "rooms": "09",
+        // rooms.toString(),
+        "price": "09",
+        // price.toString(),
+        //be careful don't delete it!!!
+        //-------------------
+        //   "type_id": typeOfApartment,
+        //-------------------
+        //be careful don't delete it!!!
+        //-------------------
+        //   "city_id": city,
+        //-------------------
+        // "type": type,
+        // "city": city,
+        // "email": email,
+        "square_meters": "09",
+        // squareMeters.toString(),
+        "title": "testing title",
+        "description":  "testing description",
+        // description.toString(),
+        "count_of_student": "09",
+        "type_id": "2",
+        "city_id": "2",
+        // countOfStudent.toString(),
+        // "owner_id":ownerId
+        // "type_id":typeId
+      });
+
+      var response = await http.post(url, headers: headers, body: body);
+      if (response.statusCode == 200) {
+        // debugPrint("apartment basic data creating...");
+
+        var json = await jsonDecode(response.body);
+
+        var apartmentId = json['data']['id'];
+        // setState(() {
+        //   apartmentIdPram = apartmentId;
+          AddAdDataContainer.id = await apartmentId;
+          debugPrint(
+              "the apartment from AddAdDataContainer is : ${AddAdDataContainer
+                  .id}");
+        // });
+        debugPrint("the new apartment Id is : $apartmentId");
+
+        // debugPrint("insert advantages is done!");
+
+        await AdvantagesModelController().insertAdvInApartment3
+          ("${await apartmentId}",
+            ['1','2','3']);
+        alert("تم اضافة أعلانك","تم إنشاء إعلان جديد ،ببيانات تم ادخالها "
+            "يدويا بالإضافة إلى إدخال قائمة من المزايا بشكل يدوي","حسنًا");
+
+
+        // });
+        // if(apartmentModelController.apartmentId != '-1'){
+        //   WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+        //   });
+        // }else{
+        //   debugPrint("apartment id is not found! ${apartmentModelController.apartmentId}");
+        // }
+
+        //
+        // debugPrint(" the apartmentIdPram is : $apartmentIdPram");
+        // debugPrint("the id of AddAdDataContainer is: ${AddAdDataContainer.id}");
+
+
+        // setState(() {
+        //   apartmentId = '-2';
+        //
+        // });
+
+
+        var res = DataOfOneApartment.fromJson(json);
+        // dataOfOneApartment = res;
+
+        // if (res.id != null) {
+        //   await advantagesModelController.insertAdvInApartment3(
+        //       "${res.id}", ["${advantagesModelController.chosen}"]);q
+        // } else {
+        //
+        //   debugPrint("your method is not working , that could not to get id "
+        //       "without retrn the res first!");
+        //
+        // }
+        // var resUser = User.fromJson(json);
+        // res. = AddAdDataContainer.countOfStudent;
+        // res.type = AddAdDataContainer.type!;
+        // res.squareMeters = AddAdDataContainer.squareMeters ?? -1;
+        // res.bathrooms = AddAdDataContainer.bathRooms ?? -1;
+        // res.rooms = AddAdDataContainer.rooms ?? -1;
+        // res.description = AddAdDataContainer.description ?? "";
+        // res.type.id = AddAdDataContainer.
+        // res.location = AddAdDataContainer.address ?? "";
+        // res.city = AddAdDataContainer.city!;
+        // res.title = AddAdDataContainer.title ?? "";
+        // res.price = AddAdDataContainer.price ?? -1;
+        // res.type?.id = readyCityAndTypeOfApartmentApi.indexApartmentType;
+        // res.city?.id = readyCityAndTypeOfApartmentApi.indexApartmentType;
+        // resUser.email = AddAdDataContainer.email;
+        // res.owner?.phone = AddAdDataContainer.user!.phone;
+        // pushToApartmentOfOwnerAfterAdd();//not nessarry
+        // if(res.id!=null){
+        //   debugPrint("the new id of aparmtant is ${res.id}");
+        //
+        // }else{
+        //   debugPrint("the id is not ready that is ${res.id}");
+        // }
+        // var apartmentSpId = (await sp).save('apartmentId');
+
+        return  res;
+
+      } else {
+        debugPrint("the statee of code is not true : ${response.statusCode}");
+        debugPrint(
+            "you have somehting null in this response :${response.body}");
+      }
+      debugPrint("the token is not null $token");
+    } else {
+      debugPrint("the token is null $token");
+    }
+    return null;
+
+    // var user = User.fromJson(json);
+
+    // ownerId = user.id as User;
+    // AddAdDataContainer.ownerId = ownerId;
+    // if (res.status == false) {
+    //   setState(() {
+    //     msg = res.msg;
+    //   });
+    //   print(json);
+    // } else {
+    //   if (res.data != null) {
+    // var data = res.data;
+  }
   void pushToNoInternetPage() {
     myPushName(context, MyPagesRoutes.noInternet);
   }
