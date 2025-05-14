5308601d7de32e5eb5baa40df7767c2633ea90d7
adhamAbuAllanMac <needforspeedun3@gmail.com>
2025-05-14 16:01:44 +0300

    enable the map on ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,8 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyD3f6FVmnF4C3XFDJinEP_iCVCUbc6vnpU")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
